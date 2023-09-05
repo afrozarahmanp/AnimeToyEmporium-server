89cca4c6a6cff85f4187bf5667e0c2c5db54f617
afrozarahmanp <afroza.rahman.p@gmail.com>
2023-09-05 12:21:10 +0600

    Load data from mongodb

--- index.js
+++ index.js
@@ -29,12 +29,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toyCollection = client.db('AnimeToys').collection('alltoys');
+
+    app.get('/alltoys', async(req, res)=>{
+      const cursor = toyCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+  })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -44,9 +55,9 @@ app.get('/', (req, res) => {
   res.send('Toys server is runnig')
 })
 
-app.get('/alltoys', (req, res) =>{
-res.send(alltoys);
-})
+// app.get('/alltoys', (req, res) =>{
+// res.send(alltoys);
+// })
 
 app.get('/alltoys/:id', (req, res) =>{
     const id = req.params.id;
